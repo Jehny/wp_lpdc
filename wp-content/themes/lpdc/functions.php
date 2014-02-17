0c123cd851ade02660fc632dfed36aeec28fad80
jehny <jehnyffen@gmail.com>
2014-02-17 17:46:58 -0300

    Ajustando Galeria e Login

--- wp-content/themes/lpdc/functions.php
+++ wp-content/themes/lpdc/functions.php
@@ -19,4 +19,45 @@ function banners_post_type() {
 			)
 	);
 }
+
+// Custom WordPress Login Logo
+function my_login_logo() { ?>
+	<style>
+	   body.login div#login h1 a {
+	        background: url('wp-content/themes/lpdc/img/logo_login.png')no-repeat;
+	        height: 70px;
+	        width: 323px;
+	        
+	   }
+ 	</style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+//Link na tela de login para a página inicial
+function my_login_logo_url() {
+	return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+	return 'Laboratório de Pesquisa em Doenças de Chagas - LPDC';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+//Custom dashboard logo
+add_action('admin_head', 'my_custom_logo');
+function my_custom_logo() {
+	echo '<style>
+ 			#wpadminbar>#wp-toolbar>#wp-admin-bar-root-default>#wp-admin-bar-wp-logo .ab-icon {
+ 				background: url('.get_bloginfo('template_directory').'/img/logo_tabless.png) no-repeat left 6px !important;
+				height: 20px;
+				width: 20px;
+				font-family: normal !important;
+				font-family: normal !important;
+				font-weight: normal !important;
+			}
+		</style>';
+}
+
+
 ?>
--- wp-content/themes/lpdc/index.php
+++ wp-content/themes/lpdc/index.php
@@ -61,10 +61,30 @@
 	    				</div> 
 	    			</li>
 	    			<li class="span4"> 
-	    				<div class="thumbnail">
+	    				<div class="thumbnail login">
 	    					<div class="labelParticoes">
 	    						<h5>Login Usuário</h5>
 	    					</div>
+	    					<?php 
+	    					 $args = array(
+	    							'echo'           => true,
+	    							'redirect'       => site_url( "wp-admin" ),
+	    							'form_id'        => 'loginform',
+	    							'label_username' => __( 'Username' ),
+	    							'label_password' => __( 'Password' ),
+	    							'label_remember' => __( 'Remember Me' ),
+	    							'label_log_in'   => __( 'Log In' ),
+	    							'id_username'    => 'user_login',
+	    							'id_password'    => 'user_pass',
+	    							'id_remember'    => 'rememberme',
+	    							'id_submit'      => 'wp-submit',
+	    							'remember'       => true,
+	    							'value_username' => NULL,
+	    							'value_remember' => false
+	    					); 
+	    					
+	    					wp_login_form($args);
+	    				?>
 	    				</div>
 	    			</li>
 				</ul>
