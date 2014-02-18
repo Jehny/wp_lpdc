b19bd7d50a8c753bd888d6566b0d21ac44b4205d
jehny <jehnyffen@gmail.com>
2014-02-18 13:32:35 -0300

    adicionando logo no footer e construindo a página de contato

--- wp-content/themes/lpdc/js/ldp.js
+++ wp-content/themes/lpdc/js/ldp.js
@@ -2,4 +2,18 @@ $(function(){
 	$('.carousel').carousel({
 	  interval: 4000
 	});
+	
+	//página de contato
+	$('.enviar').bind("click", function(){
+		if($('#nome').val() == "" || $('#email').val() == "" || $('#assunto').val() == "" || $('#descricao').val() == ""){
+			$('.alert').show();
+			console.log('show');
+		}
+	});
+	
+	$('.alert .close').bind("click", function(e) {
+		$(this).parent().hide();
+		console.log('hide');
+	});
+	
 });
